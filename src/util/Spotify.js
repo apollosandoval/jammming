8d969ae4f2de6cd00f6739e117913eea3e7dcb50
Oscar Sandoval <apollo.sandoval@gmail.com>
2018-06-18 18:08:40 -0700

    Step 88, debugging fetch call

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -13,7 +13,7 @@ const Spotify = {
         accessToken = tokenMatch[1]; // assigns the access token value returned from Spotify URL to our application
         const expiresIn = Number(expiresInMatch[1]);
         window.setTimeout(() => accessToken = '', expiresIn * 1000); // sets access token to expire after some time
-        window.history.pushState('Access Token', null, '/'); // clears parameters from URL so app doesn't try grabbing the access token after it has expired
+        window.history.pushState(accessToken, null, '/'); // clears parameters from URL so app doesn't try grabbing the access token after it has expired
         return accessToken;
       } else if (!tokenMatch) {
         const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
@@ -23,23 +23,30 @@ const Spotify = {
   }, // end of getAccessToken()
   search(term) {
     const accessToken = Spotify.getAccessToken();
-    const accessEndpoint = `https://api.spotify.com/v1/search?type=track&q=${term}`;
-    const accessHeader = {
+
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`
       }
-    };
-
-    return fetch(accessEndpoint, accessHeader)
-    .then(response => {
+    }).then(response => {
       if (response.ok) {
-        return response.json();
+        return response.json;
       }
-        throw new Error('Request failed!');
-    }, networkError => console.log(networkError.message))
-    .then(jsonResponse => {
-      //things to do with json response
-      console.log(jsonResponse);
+      throw new Error('Request failed!');
+    }, networkError => console.log(networkError.message)
+    ).then(jsonResponse => {
+      if (!jsonResponse.tracks) {
+        return [];
+      }
+      return jsonResponse.tracks.items.map(track => (
+        {
+          ID: track.id,
+          Name: track.name,
+          Artist: track.artists[0].name,
+          Album: track.album.name,
+          URI: track.uri
+        }
+      ));
     })
   } // end of search()
 };
